79211a491f13de447e432690bb583ce99b84c5c3
eaashlo <eaashlo@students.eaaa.dk>
2024-11-08 11:30:22 +0100

    Added the basic model classes

---
+++ WebGUI/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGUI.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImgURL { get; set; }
+
+        public Item(string name, double price, string imgURL)
+        {
+            Name = name;
+            Price = price;
+            ImgURL = imgURL;
+        }
+        public Item() { }
+    }
+}
---
+++ WebGUI/Models/OrderLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGUI.Models
+{
+    public class OrderLine
+    {
+        public int ID { get; set; }
+        public int Quantity { get; set; }
+        public Item Item { get; set; }
+
+        public OrderLine(int quantity, Item item)
+        {
+            Quantity = quantity;
+            Item = item;
+        }
+        public double GetTotal()
+        {
+            return Quantity * Item.Price;
+        }
+        public override string ToString()
+        {
+            return $"{Quantity} x {Item.Name} = {Item.Price}DKK.";
+        }
+    }
+}
