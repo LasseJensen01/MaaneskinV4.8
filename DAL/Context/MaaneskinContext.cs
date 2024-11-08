9795874baa5b6a35936ce360915a343d9b2737be
Lasse Jensen <eaa23laje@students.eaaa.dk>
2024-11-08 12:13:48 +0100

    EF Framwork ready for first run will merge Models into Branch

--- DAL/Context/MaaneskinContext.cs
+++ DAL/Context/MaaneskinContext.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Context {
-    internal class MaaneskinContext {
+    internal class MaaneskinContext : DbContext {
+        public MaaneskinContext() : base("Maaneskin"){
+
+        }
+
+        /*
+         * Insert various tables here like this
+         * public DbSet<Item> Items { get; set; }
+         * Make sure its plural so the OnModelCreating() function will remove the pluralization in the DB
+         * but keep it in the code
+         */
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
     }
 }
--- DAL/Context/MaaneskinInitializer.cs
+++ DAL/Context/MaaneskinInitializer.cs
@@ -6,9 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Context {
-    internal class MaaneskinInitializer : DbContext{
+    internal class MaaneskinInitializer : CreateDatabaseIfNotExists<MaaneskinContext>{
+        protected override void Seed(MaaneskinContext context) {
+            // Make an Init Data for the DB here
+            context.SaveChanges();
+        }
 
-        public class MaaneskinContext() : base("Maaneskin") {
+        //Dont fucking touch dummy or the Project wont recognize the damn DB
+        private void dummy() {
+            string result = System.Data.Entity.SqlServer.SqlFunctions.Char(65);
+        }
 
     }
 }
