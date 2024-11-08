5f5cfa83d9ee44257aaa571f435b7b64b95fa215
Lasse Jensen <eaa23laje@students.eaaa.dk>
2024-11-08 11:59:21 +0100

    init for EF

---
+++ DAL/Context/MaaneskinInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Context {
+    internal class MaaneskinInitializer : DbContext{
+
+        public class MaaneskinContext() : base("Maaneskin") {
+
+    }
+}
