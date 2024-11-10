331ac295cd46fbe4068e3e972e2e233c5cc3cfd7
LasseJensen01 <lasse9811@hotmail.dk>
2024-11-10 21:13:54 +0100

    Made TestingControllerFor DB Moving into a branch

---
+++ WebGUI/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebGUI.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            BLL.BLL.ItemBLL ItemBLL = new BLL.BLL.ItemBLL();
+            return View(ItemBLL.GetItem(0));
+        }
+    }
+}
