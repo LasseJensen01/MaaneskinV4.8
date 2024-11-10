b0aab3690958b0ed311666ef0cafa959b1b1fed2
katrine <eaa23kch@students.eaaa.dk>
2024-11-10 20:15:26 +0100

    Now possible to click an Item on the SaleHomepage and have it added to an Order object. The order is displayed on the right side of the screen

--- WebGUI/Controllers/SaleController.cs
+++ WebGUI/Controllers/SaleController.cs
@@ -8,6 +8,17 @@ namespace WebGUI.Controllers
 {
     public class SaleController : Controller
     {
+
+        private List<DTO.Models.Item> getItems()
+        {
+            //Dummy data
+            var itemModels = new List<DTO.Models.Item>();
+            itemModels.Add(new DTO.Models.Item(1, "Bajer", 10.00, "/Content/Images/CarlsbergPilsner.jpg"));
+            itemModels.Add(new DTO.Models.Item(2, "Bræz", 20.00, "/Content/Images/Alkohol.jpg"));
+
+            return itemModels;
+        }
+
         // GET: Sale
         public ActionResult SaleHomepage()
         {
@@ -15,15 +26,42 @@ namespace WebGUI.Controllers
             //var itemModels = BLL.BLL.ItemBLL.GetItems();
             //ViewBag.items = itemModels;
 
-            //Dummy data
-            var itemModels = new List<DTO.Models.Item>();
-            itemModels.Add(new DTO.Models.Item(1, "Bajer", 10.00, "/Content/Images/CarlsbergPilsner.jpg"));
-            itemModels.Add(new DTO.Models.Item(2, "Bræz", 20.00, "/Content/Images/Alkohol.jpg"));
-
-            return View("SaleHomepage", itemModels);
+            return View("SaleHomepage", getItems());
         }
 
-        [HttpPost]
+     
+        public ActionResult AddItemToOrder(int itemId)
+        {
+            var order = Session["Order"] as DTO.Models.Order;
+
+            if (order == null)
+            {
+                var orderLines = new List<DTO.Models.OrderLine>();
+                order = new DTO.Models.Order(orderLines);
+                Session["Order"] = order;
+            }
+            
+            var item = getItems().Find(x => x.ID == itemId);
+
+            //find ud af, om der allerede er en ordrelinje med varen
+            var foundOrderline = order.OrderLines.Find(x => x.Item.ID == item.ID);
+
+            if (foundOrderline != null)
+            {
+                foundOrderline.Quantity++;
+            }
+            else 
+            {
+                var orderLine = new DTO.Models.OrderLine(1, item);
+                order.OrderLines.Add(orderLine);
+            }
+
+            ViewBag.order = order;
+
+            return View("SaleHomepage", getItems());
+        }
+
+      
         public ActionResult RegisterSale()
         {
             //for each orderline in order: DB.Item =- Orderline.item.count in DB
