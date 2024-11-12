f9fa825e8dc5c87bd6648aa12b7829080bda78e8
Lasse Jensen <eaa23laje@students.eaaa.dk>
2024-11-12 08:46:55 +0100

    Changed from XUnti to NUnit testing framework

--- SpecFlowTest/StepDefinitions/CalculatorStepDefinitions.cs
+++ SpecFlowTest/StepDefinitions/CalculatorStepDefinitions.cs
@@ -1,4 +1,4 @@
-namespace SpecFlowTestItem.StepDefinitions {
+namespace SpecFlowTest.StepDefinitions {
     [Binding]
     public sealed class CalculatorStepDefinitions {
         // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
