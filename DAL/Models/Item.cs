ebb8a4f19bb4bfcf33ac859f830bbf485f0e0a59
eaashlo <eaashlo@students.eaaa.dk>
2024-11-15 09:07:46 +0100

    Added quantity to the model and DB

--- DAL/Models/Item.cs
+++ DAL/Models/Item.cs
@@ -10,17 +10,19 @@ namespace DAL.Models
     {
         [ScaffoldColumn(false)]
         public int ID { get; set; }
-
+        public int Quantity { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string ImgURL { get; set; }
         public bool IsDeleted { get; set; }
-        public Item(int id, string name, double price, string imgURL)
+        public Item(int id, string name, double price, string imgURL, int quantity)
         {
             ID = id;
             Name = name;
             Price = price;
             ImgURL = imgURL;
+            Quantity = quantity;
+
             IsDeleted = false;
         }
         public Item() { }
