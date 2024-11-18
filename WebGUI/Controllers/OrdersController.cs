f179c0f81209b0912e6cfdc282001fc3a9802b8a
katrine <eaa23kch@students.eaaa.dk>
2024-11-18 18:21:23 +0100

    Made a Controller and a View for the tab for overview of all orders. Tried to make a flexible design - ready for future changes to the model:)

---
+++ WebGUI/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebGUI.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult OrderOverview()
+        {
+            List<DTO.Models.Order> orders = new List<DTO.Models.Order>();
+
+            DTO.Models.Item cola = new DTO.Models.Item(1, "Cola", 10, "Harold.jpg", 10);
+            DTO.Models.Item øl = new DTO.Models.Item(1, "Øl", 10, "Harold.jpg", 10);
+
+            for (int i = 0; i < 5; i++)
+            {
+                DTO.Models.Order order = new DTO.Models.Order();
+
+                DTO.Models.OrderLine ol;
+
+                if (i % 2 == 0)
+                {
+                    ol = new DTO.Models.OrderLine(i, 1, cola);
+                }
+                else
+                {
+                    ol = new DTO.Models.OrderLine(i, 1, øl);
+                }
+
+                order.AddOrderLine(ol);
+                order.AddOrderLine(ol);
+                order.AddOrderLine(ol);
+                order.AddOrderLine(ol);
+                orders.Add(order);
+            }
+            
+            return View("OrderOverview", orders);
+        }
+    }
+}
