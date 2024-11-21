a9ad2e0bdc076c773c9fb2198ce0167b4135dda6
Lasse Jensen <eaa23laje@students.eaaa.dk>
2024-11-21 13:27:11 +0100

    Started test cases for order

---
+++ SpecFlowTest/Features/Order.feature
@@ -0,0 +1,10 @@
+Feature: Se en oversigt over over gemte ordre linjer i databasen
+
+	@OrderTest
+	Scenario: TC10.1 Order can be created, saved and then called from DB
+
+
+	Scenario: TC10.2 Order has the correct orderlines
+
+
+	Scenario: TC10.3 Order has valid properties
---
+++ SpecFlowTest/StepDefinitions/OrderStepDefinition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO.Models;
+
+namespace SpecFlowTest.StepDefinitions {
+    public class OrderStepDefinition {
+        private Order _order;
+        private int _id;
+        private DateTime _date;
+        private List<OrderLine> _orderlines = new List<OrderLine>();
+
+        [Given (@"Parameters, ID (.*), Date (.*), Orderlines (.*)")]
+        public void GivenCorrectParameters(int id, DateTime date, List<OrderLine> orderLines) {
+            _id = id;
+            _date = date;
+            _orderlines = orderLines;
+        }
+
+        [When (@"I create order")]
+        public void WhenICreateOrder() {
+            _order = new Order(_id);
+            _order.Date = _date;
+            _order.OrderLines = _orderlines;
+        }
+    }
+}
