8ef15197b99648be8b3f49696a45021abcdf5d69
katrine <eaa23kch@students.eaaa.dk>
2024-11-22 11:31:58 +0100

    Made two TotalPrice() metods for order in order to be able to display different totals in sale and order

--- DTO/Models/Order.cs
+++ DTO/Models/Order.cs
@@ -26,12 +26,22 @@ namespace DTO.Models
         public bool RemoveOrderline(OrderLine orderLine) {
             return this.OrderLines.Remove(orderLine);
         }
-        public double GetTotal()
+        public double GetTotalOrderPrice()
         {
             double total = 0;
             foreach (OrderLine orderLine in OrderLines)
             {
                 total += orderLine.TotalPrice;
+            }
+            return total;
+        }
+
+        public double GetTotalSalePrice()
+        {
+            double total = 0;
+            foreach (OrderLine orderLine in OrderLines)
+            {
+                total += orderLine.GetTotal();
             }
             return total;
         }
@@ -42,7 +52,7 @@ namespace DTO.Models
             {
                 orderString += orderLine.ToString() + "\n";
             }
-            orderString += $"Total: {GetTotal()} kr.";
+            orderString += $"Total: {GetTotalSalePrice()} kr.";
             return orderString;
         }
     }
