f853ece2d377cd1aee9ca86ccfd27570737ad353
katrine <eaa23kch@students.eaaa.dk>
2024-11-22 01:52:10 +0100

    Made a mapper for list of Orders

--- DAL/Mapper/OrderMapper.cs
+++ DAL/Mapper/OrderMapper.cs
@@ -1,4 +1,5 @@
 ﻿using DAL.Models;
+using DTO.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,18 +8,18 @@ using System.Threading.Tasks;
 
 namespace DAL.Mapper {
     public class OrderMapper {
-        public static DTO.Models.Order Map(Order order) {
+        public static DTO.Models.Order Map(DAL.Models.Order order) {
             DTO.Models.Order retur = new DTO.Models.Order(order.ID);
             retur.Date = order.Date;
-            foreach(OrderLine orderline in order.OrderLines) {
+            foreach(DAL.Models.OrderLine orderline in order.OrderLines) {
                 retur.AddOrderLine(OrderLineMapper.Map(orderline));
             }
             
             return retur;
         }
 
-        public static Order Map(DTO.Models.Order order) {
-            Order retur = new Order(order.ID);
+        public static DAL.Models.Order Map(DTO.Models.Order order) {
+            DAL.Models.Order retur = new DAL.Models.Order(order.ID);
             order.Date = retur.Date;
             foreach(DTO.Models.OrderLine orderline in order.OrderLines) {
                 retur.AddOrderLine(OrderLineMapper.Map(orderline));
@@ -27,15 +28,24 @@ namespace DAL.Mapper {
 
         }
 
-        public static void Update(DTO.Models.Order order, Order dataOrder) {
+        public static void Update(DTO.Models.Order order, DAL.Models.Order dataOrder) {
             //TODO
         }
 
-        public static List<DTO.Models.Order> Map(List<Order> orders) {
-            //TODO
+        public static List<DTO.Models.Order> Map(List<DAL.Models.Order> orders) {
+            List<DTO.Models.Order> newOrders = new List<DTO.Models.Order>();
+            if (orders != null && orders.Count >= 0)
+            {
+                foreach (DAL.Models.Order order in orders)
+                {
+                    newOrders.Add(Map(order));
+                }
+                return newOrders;
+            }
+
             return null;
         }
-        public static List<Order> Map(List<DTO.Models.Order> orders) {
+        public static List<DAL.Models.Order> Map(List<DTO.Models.Order> orders) {
             //TODO
             return null;
         }
